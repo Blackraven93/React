e80804d3159df3822392546acea63c263fb17605
곽세진_2004_anl <Reblackraven@Gmail.com>
2021-10-12 20:22:41 +0900

    practice event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import MyComponent from "./components/MyComponent";
+import Counter from "./components/Counter";
+import EventPractice from "./components/EventPractice";
+import PropTypes from "prop-types";
 
 function App() {
+  const name = "React";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {name === "React" && <h1>리액트 입니다.</h1>}
+      <MyComponent name="raven">리액트</MyComponent>
+      <MyComponent children="React" />
+      <Counter></Counter>
+      <EventPractice></EventPractice>
+    </>
   );
 }
 
