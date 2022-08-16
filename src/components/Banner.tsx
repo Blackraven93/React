1c418c56f3a0dbc1eb9bb4dc837fc7d974a13145
Blackraven93 <reblackraven@gmail.com>
2022-08-16 23:51:31 +0900

    Feat: add request about movie detail

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,6 +7,13 @@ function Banner() {
   const fetchData = async () => {
     const request = await axios.get(requests.fetchNowPlaying);
     console.log(request);
+    const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: {append_to_response: 'videos'},
+    });
+
+    setMovie(movieDetail)
   };
 
   useEffect(() => {
