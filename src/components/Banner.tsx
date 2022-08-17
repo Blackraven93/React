d21514367faf7770e1c3fa7031a5ea5eea16f6d6
Blackraven93 <reblackraven@gmail.com>
2022-08-17 23:55:19 +0900

    Feat: create Banner View

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,17 +3,27 @@ import axios from '../api/axios';
 import requests from '../api/requests';
 
 function Banner() {
-  const [movie, setMovie] = useState([]);
+  interface IMovie {
+    title: string;
+    name: string;
+    original_name?: string;
+    overview?: string;
+  }
+
+  const [movie, setMovie] = useState<IMovie | Array<null>>([]);
   const fetchData = async () => {
     const request = await axios.get(requests.fetchNowPlaying);
     console.log(request);
-    const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
 
     const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
-      params: {append_to_response: 'videos'},
+      params: { append_to_response: 'videos' },
     });
 
-    setMovie(movieDetail)
+    setMovie(movieDetail);
   };
 
   useEffect(() => {
@@ -21,7 +31,32 @@ function Banner() {
     fetchData();
   }, []);
 
-  return <div>Banner</div>;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name || ''}
+        </h1>
+        <div className="banner__buttons">
+          <button type="button" className="banner__button play">
+            Play
+          </button>
+          <button type="button" className="banner__button info">
+            <div className="space" /> More Information
+          </button>
+        </div>
+        <h1 className="banner__description">{movie?.overview || ''}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 }
 
 export default Banner;
