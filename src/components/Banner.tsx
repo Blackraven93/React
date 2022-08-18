43ac58aae02782345330625521e09c97be59f80e
Blackraven93 <reblackraven@gmail.com>
2022-08-18 23:38:46 +0900

    Style: add Banner.css

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
 import requests from '../api/requests';
+import './styles/Banner.css';
 
 function Banner() {
   interface IMovie {
