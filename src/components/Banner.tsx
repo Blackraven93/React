367af2612c2879b2311511caeb814d97879080c2
Blackraven93 <reblackraven@gmail.com>
2022-08-21 23:44:01 +0900

    Style: create Iframe component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -74,7 +74,13 @@ function Banner() {
     return (
       <Container>
         <HomeContainer>
-          
+          <IFrame
+            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          ></IFrame>
         </HomeContainer>
       </Container>
     )
@@ -93,6 +99,22 @@ const Container = styled.div`
 const HomeContainer = styled.div`
   width: 100%;
   height: 100%;
+`
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 `
 
 export default Banner;
