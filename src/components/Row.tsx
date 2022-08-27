7d4f2d6296ff6fe6519a6e54d10c48b5d95cee5c
Blackraven93 <reblackraven@gmail.com>
2022-08-27 23:45:59 +0900

    Feat: request to fetchUrl using axios and useEffect

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 interface IRow {
   title: string;
@@ -8,6 +9,16 @@ interface IRow {
 }
 
 function Row({ title, fetchUrl, isLargeRow, id }: IRow) {
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      const request = await axios.get(fetchUrl)
+      
+    }
+    fetchMovieData();
+  }, [])
+  
+
   return (
     <div>
       <div>{title}</div>
