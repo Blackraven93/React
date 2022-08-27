63710e69f9941556602906260a36efb5f2152585
Blackraven93 <reblackraven@gmail.com>
2022-08-27 23:53:45 +0900

    Style: add Row component UI

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface IRow {
   title: string;
@@ -9,23 +9,41 @@ interface IRow {
 }
 
 function Row({ title, fetchUrl, isLargeRow, id }: IRow) {
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchMovieData = async () => {
-      const request = await axios.get(fetchUrl)
-      
-    }
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    };
     fetchMovieData();
-  }, [])
-  
+  }, []);
 
   return (
-    <div>
-      <div>{title}</div>
-      <div>{fetchUrl}</div>
-      <div>{isLargeRow}</div>
-      <div>{id}</div>
-    </div>
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => {
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${BASE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />;
+          })}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
   );
 }
 
